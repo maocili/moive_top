f2fdc18b86c77de83e11723acdf6339309eba837
maocili <maocili@163.com>
2021-06-21 12:27:24 +0800

    产品盒子结构调整，2行3列，添加文字描述

--- js/produce_box.js
+++ js/produce_box.js
@@ -1,13 +1,19 @@
 function produce_img_over(e) {
     sm_img = e.getElementsByClassName("product_img_small");
     wd_img = e.getElementsByClassName("product_img_width");
+    produce_text = e.getElementsByClassName("produce_text");
     sm_img[0].style.display = "none";
     wd_img[0].style.display = "block";
+    produce_text[0].style.display = "block"
 }
 
 function produce_img_out(e) {
     sm_img = e.getElementsByClassName("product_img_small");
     wd_img = e.getElementsByClassName("product_img_width");
+    produce_text = e.getElementsByClassName("produce_text");
+
     wd_img[0].style.display = "none";
     sm_img[0].style.display = "block";
+    produce_text[0].style.display = "none"
+
 }
